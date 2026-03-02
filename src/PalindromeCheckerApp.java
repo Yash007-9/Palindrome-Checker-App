3be347007e4e9f033857776987858edb0d633ab1
Yash007-9 <ys8737@srmist.edu.in>
2026-03-02 15:49:07 +0530

    UC2: User input palindrome checker

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,28 +1,18 @@
 import java.util.Scanner;
 
 public class PalindromeCheckerApp {
-    // Method to check palindrome
-    public static boolean isPalindrome(String s) {
-        int left = 0, right = s.length() - 1;
-        while (left < right) {
-            if (s.charAt(left) != s.charAt(right))
-                return false;
-            left++;
-            right--;
-        }
-        return true;
-    }
-
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter string to check palindrome: ");
+
+        System.out.print("Enter a string to check palindrome: ");
         String input = scanner.nextLine();
 
-        if (isPalindrome(input)) {
+        if (input.equals(new StringBuilder(input).reverse().toString())) {
             System.out.println("\"" + input + "\" is a palindrome.");
         } else {
             System.out.println("\"" + input + "\" is not a palindrome.");
         }
+
         scanner.close();
     }
 }
