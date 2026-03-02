fe787bd179b25d1615478a8190b5bb7e95dc60f1
Yash007-9 <ys8737@srmist.edu.in>
2026-03-02 15:58:28 +0530

    UC3: Palindrome check using string reverse method

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class UC3StringReverse {
+public class PalindromeCheckerApp {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
